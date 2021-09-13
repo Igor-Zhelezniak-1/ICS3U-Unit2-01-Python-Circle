4a967683d70af6dd63ea34486639d465f484c2ce
Igor <ihor.zhelezniak@mths.ca>
2021-09-13 17:37:41 +0000

    aded circle program

---
+++ circle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Igor
+# Created on: Sept 2021
+# This is math program
+
+import math
+
+def main():
+    # This is math program
+    print("If a circle has radius 15 mm:")
+    print("Area = {}".format(math.pi * (15**2)))
+    print("diameter = {}".format(2*15))
+    print("Done")
+
+if __name__ == "__main__":
+    main()
